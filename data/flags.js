9379f178db3c9f827f56a2ebb97e562442913c28
kaafinski <szymonpel6@gmail.com>
2022-07-17 10:25:58 +0200

    Flags API

---
+++ data/flags.js
@@ -0,0 +1,78 @@
+module.exports = [
+    {
+        flag: "🇦🇨",
+        name: ["ascension island"]
+    },
+    {
+        flag: "🇦🇩",
+        name: ["andorra", "andora"]
+    },
+    {
+        flag: "🇦🇪",
+        name: ["united arab emirates", "arab emirates"]
+    },
+    {
+        flag: "🇦🇫",
+        name: ["afghanistan", "afganistan"]
+    },
+    {
+        flag: "🇦🇬",
+        name: ["antigua & barbuda", "antigua and barbuda"]
+    },
+    {
+        flag: "🇦🇮",
+        name: ["anguilla"]
+    },
+    {
+        flag: "🇦🇱",
+        name: ["albania"]
+    },
+    {
+        flag: "🇦🇲",
+        name: ["armenia"]
+    },
+    {
+        flag: "🇦🇴",
+        name: ["angola"]
+    },
+    {
+        flag: "🇦🇶",
+        name: ["antarctica"]
+    },
+    {
+        flag: "🇦🇷",
+        name: ["argentina"]
+    },
+    {
+        flag: "🇦🇸",
+        name: ["american samoa"]
+    },
+    {
+        flag: "🇦🇹",
+        name: ["austria"]
+    },
+    {
+        flag: "🇦🇺",
+        name: ["australia"]
+    },
+    {
+        flag: "🇦🇼",
+        name: ["aruba"]
+    },
+    {
+        flag: "🇦🇿",
+        name: ["azerbaijan"]
+    },
+    {
+        flag: "🇧🇦",
+        name: ["bosnia", "herzegovina", "bosnia & herzegovina", "bosnia and herzegovina"]
+    },
+    {
+        flag: "🇧🇧",
+        name: ["barbados"]
+    },
+    {
+        flag: "🇧🇩",
+        name: ["bangladesh"]
+    }
+]
--- routes/Endpoints.js
+++ routes/Endpoints.js
@@ -2,9 +2,17 @@ const express = require('express')
 const router = express.Router()
 const logger = require('../functions/Logger')
 
-router.get('/endpoints', (req, res) => {
+function random(array) {
+    const randomItem = Math.floor(Math.random() * array.length);
+    return array[randomItem];
+}
+
+router.get('/flags', (req, res) => {
+    let flags = require('../data/flags')
+    res.status(200)
     res.send({
-        message: "The list of endpoints is now available on main page!"
+        code: 200,
+        message: random(flags),
     })
 })
 
