6bb8fb8a9b6f226ac950f5e1da79b0750253c97d
kaafinski <szymonpel6@gmail.com>
2022-07-17 14:29:50 +0200

    Website update

--- routes/Website.js
+++ routes/Website.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const logger = require('../functions/Logger')
-const path = require('path')
-
 
 router.get("/", async (req, res) => {
-    const options = { 
-      root: path.join(__dirname, `../website`)
-    }
-  res.sendFile(`index.html`, options)
-  });
+    let endpoints = 0;
+    require('./Endpoints').stack.forEach(() => {
+        endpoints += 1
+    })
+    res.render("main", {
+        endpoints: endpoints
+    })
+});
 
 
 logger.log("Website.js loaded.")
