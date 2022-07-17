15f68623e7a9f8f72b9e2037880b2476c2b9c087
kaafinski <szymonpel6@gmail.com>
2022-07-17 09:29:29 +0200

    Added logger + appliaction is now working

--- functions/Logger.js
+++ functions/Logger.js
@@ -5,3 +5,11 @@ const date = moment().format("[[]DD-MM-YYYY ss:mm:HH[]]")
 module.exports.log = (content) => {
     console.log(chalk.gray(date), chalk.green.bold("LOG"), content)
 }
+
+module.exports.error = (content) => {
+    console.log(chalk.gray(date), chalk.red.bold("ERROR"), content)
+}
+
+module.exports.warn = (content) => {
+    console.log(chalk.gray(date), chalk.yellow.bold("WARN"), content)
+}
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const { port } = require('./data/config.json')
+const logger = require('./functions/Logger')
 
 app.set('views', path.join(__dirname) + '/views')
 app.set('view engine', 'hbs')
@@ -10,5 +11,6 @@ app.use('/', require('./routes/Website'))
 app.use('/api', require('./routes/Endpoints'))
 
 app.listen(port, e => {
-    if(e) return 
+    if(e) logger.error(`Error occured.\n${e}`)
+    else logger.log(`Successfully started plabs api! Working on port ${port}`)
 })
